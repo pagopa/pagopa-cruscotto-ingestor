c8d678b2e87d26d5b93e478f3f9d9f77bef9d9d7
CO03675 <marco.colosi@accenture.com>
2026-05-18 17:39:08 +0200

    2nd commit Signed-off-by: CO03675 <marco.colosi@accenture.com>

--- src/main/java/it/pagopa/cruscotto/ingestion/repository/PositionTransfersRepository.java
+++ src/main/java/it/pagopa/cruscotto/ingestion/repository/PositionTransfersRepository.java
@@ -2,14 +2,19 @@ package it.pagopa.cruscotto.ingestion.repository;
 
 import it.pagopa.cruscotto.ingestion.entity.PositionTransfers;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface PositionTransfersRepository extends JpaRepository<PositionTransfers, Integer> {
-	@Query("SELECT pt FROM PositionTransfers pt WHERE pt.fkToken = ?1 AND pt.paTransfer = ?2 AND pt.idTransfer = ?3 " +
-			"ORDER BY pt.id DESC LIMIT 1")
-	Optional<PositionTransfers> findLatestByTokenAndTransferId(Integer fkToken, String paTransfer, Short idTransfer);
+	Optional<PositionTransfers> findFirstByFkTokenAndPaTransferAndIdTransferOrderByIdDesc(
+			Integer fkToken,
+			String paTransfer,
+			Short idTransfer
+	);
+
+	default Optional<PositionTransfers> findLatestByTokenAndTransferId(Integer fkToken, String paTransfer, Short idTransfer) {
+		return findFirstByFkTokenAndPaTransferAndIdTransferOrderByIdDesc(fkToken, paTransfer, idTransfer);
+	}
 }
